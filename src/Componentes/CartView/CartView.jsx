7bfa9e9cee4914800b38245ca30212fde10f5049
Leandro Yurakoski <lyurakoski@hotmail.com>
2022-10-08 20:19:19 -0300

    totalPrice

--- src/Componentes/CartView/CartView.jsx
+++ src/Componentes/CartView/CartView.jsx
@@ -1,12 +1,24 @@
-
-import React, {useContext} from 'react'
-import { Context } from "../CartContext/CartContext";
+import React, {useContext, useState, useEffect} from 'react'
+import {Context} from "../CartContext/CartContext";
 import {Link} from 'react-router-dom'
 
 const CartView = () => {
 
     const {cart, removeItem} = useContext(Context);
     console.log(cart);
+
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    const totalPriceCart = ()=>{
+        const priceItems = cart.map(item => item.product.price * item.counter);
+        const price= priceItems.reduce((sum, item) => sum + item, 0);
+        setTotalPrice(price)
+    }
+
+    useEffect(() => {
+        totalPriceCart();
+    }, [cart]);
+
     return(
         <>
         {cart.length === 0 ? (<div>No agregaste ningún producto, para ver los productos, haz click <Link to="/">aquí</Link></div>) :
@@ -14,9 +26,10 @@ const CartView = () => {
             {cart.map((prod) => {
                 return <>
                         <div key={prod.product.id}>Producto: {prod.product.title} CANTIDAD: {prod.counter}</div>
-                        <button key={`${prod.product.id}-${prod.product.title}`} onClick={()=>removeItem(prod.product.id)}>borrar</button>
+                        <button key={`${prod.product.id}-${prod.product.title}`} onClick={()=>removeItem(prod.product.id)}>Borrar</button>
                     </>
                 })}
+                <div>TOTAL: ${totalPrice}</div>
          </>)}
         </>
     )
